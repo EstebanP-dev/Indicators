a91a8437dafd933ed4e5a795382b4581006d5404
juanestebannaviaperez <soporte@doctus.com.co>
2023-10-19 08:19:50 -0500

    Add indicator configuration.

---
+++ api/src/IndicatorsApi.Persistence/Features/Indicators/Indicator.cs
@@ -0,0 +1,45 @@
+﻿using IndicatorsApi.Domain.Features.Indicators;
+using IndicatorsApi.Persistence.Abstractions;
+
+namespace IndicatorsApi.Persistence.Features.Indicators;
+
+/// <inheritdoc/>
+internal sealed class IndicatorRepository
+    : Repository<Indicator, int>, IIndicatorRepository
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IndicatorRepository"/> class.
+    /// </summary>
+    /// <param name="context">Instance of <see cref="ApplicationDbContext"/>.</param>
+    public IndicatorRepository(ApplicationDbContext context)
+        : base(context)
+    {
+    }
+}
+
+/// <inheritdoc/>
+internal sealed class IndicatorConfiguration
+    : IEntityTypeConfiguration<Indicator>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<Indicator> builder)
+    {
+        builder
+            .ToTable("indicador");
+
+        builder
+            .HasKey(x => x.Id);
+
+        builder
+            .Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder
+            .Property(x => x.Code)
+            .HasColumnName("codigo")
+            .HasMaxLength(50)
+            .IsRequired();
+    }
+}
